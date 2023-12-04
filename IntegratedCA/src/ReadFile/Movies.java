5048361f5539e7a3a8b427106c966e3a98775dc0
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-12-04 20:47:03 +0000

    Add Movies class to get and set the title and price

---
+++ IntegratedCA/src/ReadFile/Movies.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ReadFile;
+
+/**
+ *
+ * @author Wandwilson Almeida 2021230
+ */
+public class Movies {
+    private String title;
+    private double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
