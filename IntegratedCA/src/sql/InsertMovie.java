72bf48c0ca0e7ad2c231440928358a4d8418711a
Carolina <carolina.gomeslandim@gmail.com>
2023-12-08 09:38:36 +0000

    Changing the name of the class
    Changing the name class to a better understanding of what this class do

--- IntegratedCA/src/sql/InsertMovie.java
+++ IntegratedCA/src/sql/InsertMovie.java
@@ -4,7 +4,7 @@
  */
 package sql;
 
-import UserGetSet.User;
+import ReadFile.Movies;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -14,17 +14,18 @@ import sql.ConnectionRTE;
  *
  * @author carol
  */
-public class GetMovie implements GetMovieInterface {
-      Connection connect;
+public abstract class InsertMovie implements InsertMovieInterface {
+    
+    Connection connect;
     PreparedStatement pstm;
     /* 
-        Insert the Movies and prices on the DataBase.
+        Insert the movies and prices on the DataBase.
     */
 
     @Override
-    public void GetMovie(User objgetmovie){
+    public void InsertMovie(Movies objgetmovie){
        
-       String SQL_COMMAND = "insert into Movies (movie , price (?, ?)";// insert the new movie and price on the DataBase.
+       String SQL_COMMAND = "insert into movies (title , price ) values (?, ?)";// insert the new name movie and price on the DataBase.
 
         connect = new ConnectionRTE().connectDB();
 
@@ -34,8 +35,8 @@ public class GetMovie implements GetMovieInterface {
             required information : movie and price.
             */
             pstm = connect.prepareStatement(SQL_COMMAND);
-            pstm.setString(1, objgetmovie.getMovie());// set and get the movie.
-            pstm.setString(2, objgetmovie.getPrice());// set and get the price.
+            pstm.setString(1, objgetmovie.getTitle());// set and get the movie.
+            pstm.setDouble(2, objgetmovie.getPrice());// set and get the price.
             
             pstm.execute("USE rteplayer;");
             pstm.execute();
@@ -44,5 +45,6 @@ public class GetMovie implements GetMovieInterface {
             System.out.println("Get Movie: " + e);
         }
     }
+
     
 }
