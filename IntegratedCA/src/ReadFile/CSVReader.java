d5e165da6837c21ad1b0d882c02551c29e0c06dd
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-12-10 13:49:18 +0000

    Update CSVReader.java

--- IntegratedCA/src/ReadFile/CSVReader.java
+++ IntegratedCA/src/ReadFile/CSVReader.java
@@ -20,63 +20,63 @@ import sql.InsertMovie;
 public class CSVReader extends FileIO implements CSVReaderInterface {
     private int numColumns;
     private String[] header;
-    Map<String, Integer> columnIndexMap;
-    CheckMovies checkMovies = new CheckMovies();
-    InsertMovie insertMovie = new InsertMovie();
-    Movies movie = new Movies();
+    private Map<String, Integer> columnIndexMap = new HashMap<>();
+    private CheckMovies checkMovies = new CheckMovies();
+    private InsertMovie insertMovie = new InsertMovie();
+    private Movies movie = new Movies();
     
     @Override
     public ArrayList<Movies> readData() {
-        ArrayList<Movies> movies = new ArrayList<>();
+     ArrayList<Movies> movies = new ArrayList<>();
 
-        try (Scanner readFile = new Scanner(new File(fileName + ".csv"))) {
-            // Read the header line
-            if (readFile.hasNextLine()) {
-                String headerLine = readFile.nextLine();
-                header = CSVUtils.splitCSVLine(headerLine);
-                numColumns = header.length;
+     try (Scanner readFile = new Scanner(new File(fileName + ".csv"))) {
+         // Read the header line
+         if (readFile.hasNextLine()) {
+             String headerLine = readFile.nextLine();
+             header = CSVUtils.splitCSVLine(headerLine);
+             numColumns = header.length;
 
-                // Map column names to indices
-                columnIndexMap = new HashMap<>();
-                for (int i = 0; i < numColumns; i++) {
-                    columnIndexMap.put(header[i], i);
-                }
-            }
-            // Process the lines and create Movie objects
-            while (readFile.hasNextLine()) {
-                movie = new Movies(); // Create a new Movies object for each line
-                String line = readFile.nextLine();
+             // Map column names to indices
+             columnIndexMap = new HashMap<>();
+             for (int i = 0; i < numColumns; i++) {
+                 columnIndexMap.put(header[i], i);
+             }
+         }
+         // Process the lines and create Movie objects
+         while (readFile.hasNextLine()) {
+             movie = new Movies(); // Create a new Movies object for each line
+             String line = readFile.nextLine();
 
 
-                if (line.trim().isEmpty() || line.trim().equals(",,,,,,,,,,")) {
-                    break; // Exit the loop if the line is empty (end of file)
-                }
+             if (line.trim().isEmpty() || line.trim().equals(",,,,,,,,,,")) {
+                 break; // Exit the loop if the line is empty (end of file)
+             }
 
-                String[] values = CSVUtils.splitCSVLine(line);
+             String[] values = CSVUtils.splitCSVLine(line);
 
-                movie.setTitle(values[columnIndexMap.get("original_title")].trim()); // Column B (index 1)
+             movie.setTitle(values[columnIndexMap.get("original_title")].trim()); // Column B (index 1)
 
-                // Handle the value of column K as price only if it's a number
-                String priceValue = values[columnIndexMap.get("price")].trim();
+             // Handle the value of column K as price only if it's a number
+             String priceValue = values[columnIndexMap.get("price")].trim();
 
-                double price = Double.parseDouble(priceValue);
-                movie.setPrice(price); // Column K (index 10)
-                movies.add(movie);
-            }
+             double price = Double.parseDouble(priceValue);
+             movie.setPrice(price); // Column K (index 10)
+             movies.add(movie);
+         }
 
-            // Check if the movies already exist in the database
-            if (!checkMovies.CheckMovies(movies)) {
-                // Insert into the database
-                insertMovie.InsertMovie(movies);
-            }
-           
-        } catch (FileNotFoundException e) {
-            System.out.println("The file: " + fileName + ".csv could not be located.");
-    
-        }
+         // Check if the movies already exist in the database
+         if (!checkMovies.CheckMovies(movies)) {
+             // Insert into the database
+             insertMovie.InsertMovie(movies);
+         }
 
-        return movies;
-    }
+     } catch (FileNotFoundException e) {
+         System.out.println("The file: " + fileName + ".csv could not be located.");
+
+     }
+
+     return movies;
+ }
     
     @Override
     public int getNumColumns() {
