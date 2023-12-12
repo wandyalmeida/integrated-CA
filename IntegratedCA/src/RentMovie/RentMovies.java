f3a30209c7ec958a8afa652be33efeea10bf70bc
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-12-12 07:08:54 +0000

    Update the RentMovies class

--- IntegratedCA/src/RentMovie/RentMovies.java
+++ IntegratedCA/src/RentMovie/RentMovies.java
@@ -3,9 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package RentMovie;
+import ReadFile.Movies;
 import java.util.Scanner;
 import sql.InsertMovieChart;
-import sql.SeeMovies;
+import sql.SelectMovie;
 import sql.ShowMenu;
 
 
@@ -19,32 +20,35 @@ public class RentMovies {
      * collect some input from the user to rent the movie.
     **/
        
-    public void RentMovies(){
+    public void RentMovies(int userID){
         Scanner userInput = new Scanner(System.in);
         int movieId, rentConfirm;
-//        SeeMovies movies = new SeeMovies();
         ShowMenu menu = new  ShowMenu();
         
-        System.out.println("Please enter the ID of the movie you would like to rent");
+        System.out.println("Please enter the ID of the movie you would like to rent: ");
         movieId = userInput.nextInt();
+        Movies movie = new Movies();
+        movie.setMovie_id(movieId);
+        SelectMovie selectMovie = new SelectMovie();
+        selectMovie.selectMovie(movie);
         
-        System.out.println("Please can you confirm are you rent this movie " + movieId);
+        System.out.println("Please can you confirm that you are renting this movie " + movieId);
         System.out.println("1[Yes] \n2[No]");
         rentConfirm = userInput.nextInt();
         switch (rentConfirm){
             case 1:
                 System.out.println("Movie rented"); 
                 InsertMovieChart moviechart = new InsertMovieChart();
-                moviechart.InsertMovieChart(1, movieId);
-                menu.showMenu(); 
+                moviechart.InsertMovieChart(userID, movieId);
+                menu.showMenu(userID); 
                 break;
             case 2:
                 System.out.println("Movie not rented");
-                menu.showMenu();
+                menu.showMenu(userID);
                 break;
             default:
                 System.out.println("Sorry wrong option. Try again..");
-                RentMovies();
+                RentMovies(userID);
                 
                 
         }
