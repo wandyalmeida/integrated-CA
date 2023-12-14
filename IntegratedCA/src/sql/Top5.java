bf9dd52971602354fa0b218ee775695ab8cb5925
Carolina <carolina.gomeslandim@gmail.com>
2023-12-14 18:00:05 +0000

    Update Top5.java

--- IntegratedCA/src/sql/Top5.java
+++ IntegratedCA/src/sql/Top5.java
@@ -4,12 +4,9 @@
  */
 package sql;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import sql.ConnectionRTE;
-
 /**
  *
  * @author Carolina Landim 2021226
@@ -17,31 +14,21 @@ import sql.ConnectionRTE;
  * This class it is to stored the most selected movies.
  * 
  */
-public class Top5 implements Top5Interface {
-    
-    /*
-    Methods command
-    Connection 
-    Prepared Statement 
-    */
-    String SQL_COMMAND;
-    Connection connect;
-    PreparedStatement pstm;
+public class Top5 extends Attributes implements Top5Interface {
     
     @Override
     public void Top5(){
       try {
-            connect = new ConnectionRTE().connectDB();
             pstm = connect.prepareStatement(SQL_COMMAND);
             // Check the top 5 movies in the database
-            String query = "SELECT movies.title, movies.price, COUNT(movie_chart.movie_id) AS selections " +
+            SQL_COMMAND = "SELECT movies.title, movies.price, COUNT(movie_chart.movie_id) AS selections " +
                            "FROM movie_chart " +
                            "JOIN movies ON movie_chart.movie_id = movies.movie_id " +
                            "GROUP BY movie_chart.movie_id " +
                            "ORDER BY selections DESC " +
                            "LIMIT 5";
 
-            PreparedStatement preparedStatement = connect.prepareStatement(query);
+            PreparedStatement preparedStatement = connect.prepareStatement(SQL_COMMAND);
             ResultSet rs = preparedStatement.executeQuery();
  
             // This will be the results of the top 5 of movies
