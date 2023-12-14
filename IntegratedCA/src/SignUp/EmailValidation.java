3e46f7c75c93fbe5eb6b5adc8b4685749431af6b
Charles Rocha <charlesmsrocha@gmail.com>
2023-12-14 16:13:46 +0000

    Fixing and commenting EmailValidation.

--- IntegratedCA/src/SignUp/EmailValidation.java
+++ IntegratedCA/src/SignUp/EmailValidation.java
@@ -1,6 +1,5 @@
 package SignUp;
 
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,69 +8,35 @@ import java.util.regex.Pattern;
  * @author Charles Rocha
  */
 public class EmailValidation {
-
-    private static final String emailToBeValidated = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
+    
+    /**
+     * The following regular expression validates an email address with the following criteria:
+     * The local part (before '@') allows letters, numbers, and some special characters;
+     * It can have optional sub-domains separated by dots in the local part;
+     * The '@' character is required;
+     * The domain part (after '@') allows letters, numbers, and hyphens, with at least one dot separating sub-domains;
+     * The country code part has between 2 and 7 alphabetical characters. 
+     */
+    
+    private static final String emailToBeValidated = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
     private static Pattern pattern;
     private static Matcher matcher;
-    Scanner emailScanner = new Scanner(System.in);
-    String input = "", inputToLowerCase;
 
-    public EmailValidation() {
+    /*
+     * The constructor initializes a Pattern object with a regular expression for email validation, 
+     * and the Pattern.CASE_INSENSITIVE flag is set to make the pattern matching case-insensitive
+     */
+        public EmailValidation() {
         pattern = Pattern.compile(emailToBeValidated, Pattern.CASE_INSENSITIVE);
     }
 
-    public boolean email_validator(String email_address) {
+        /*
+        * This method takes an email address as input, creates a Matcher object using the compiled pattern, 
+        * and then checks if the entire email address matches the pattern. 
+        */
+        public boolean emailValidator(String email_address) {
         matcher = pattern.matcher(email_address);
         return matcher.matches();
     }
 
-    public String get_user_valid_input(String prompt) {
-
-        do {
-            System.out.println(prompt);
-            System.out.println("Only type y or n, please: ");
-            input = emailScanner.nextLine();
-            inputToLowerCase = input.toLowerCase();
-
-        } while ((!inputToLowerCase.equals("y")) && (!inputToLowerCase.equals("n")));
-        return input;
-
-    }
-
-    public String get_user_input(String prompt) {
-//        Scanner emailScanner = new Scanner(System.in);
-        String input = "";
-        do {
-            System.out.println(prompt + " ");
-            System.out.print("Please only type letters: ");
-            input = emailScanner.nextLine();
-            System.out.println("");
-        } while (!input.matches("[a-zA-Z ]+"));
-
-        return input;
-    }
-
-    public int Get_user_int(String prompt, int minValue, int maxValue) {
-//        Scanner emailScanner = new Scanner(System.in);
-        int input = -1;
-        boolean valid = false;
-        do {
-            System.out.print(prompt);
-            try {
-                input = emailScanner.nextInt();
-                if (input < minValue || input > maxValue) {
-                    System.out.println("The number entered does not match requirements. Please, try again:");
-                } else {
-
-                    valid = true;
-                }
-            } catch (Exception e) {
-                System.out.println("Error initial Screen: " + e);
-                System.out.println("The value entered is not an integer. Please type only 1 or 2");
-                emailScanner.next();
-            }
-        } while (!valid);
-        System.out.println("");
-        return (input);
-    }
 }
