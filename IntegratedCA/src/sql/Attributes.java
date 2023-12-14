406b7c24ca5b1605956d729eb0a0ab7a7075d2ff
Carolina <carolina.gomeslandim@gmail.com>
2023-12-14 19:02:51 +0000

    Update Attributes.java

--- IntegratedCA/src/sql/Attributes.java
+++ IntegratedCA/src/sql/Attributes.java
@@ -4,10 +4,28 @@
  */
 package sql;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
- * @author carol
+ * @author Carolina Landim
+ * 
+ * This class represent all attributes that will be used in another classes
+ * in the sql packages
  */
 public class Attributes {
+    
+   protected String SQL_COMMAND;
+   protected  Connection connect = new ConnectionRTE().connectDB();
+   protected  PreparedStatement pstm;
+   protected  ResultSet rs;
+   protected String database = "USE rteplayer";
+    
+    
+          
+        
+    
     
 }
